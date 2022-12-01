f1d256522bcd7099dba9660e8259e7297287d923
Johannes Katzenbeisser <jkatzenb@c2r3p8.42vienna.com>
2022-12-01 14:51:39 +0100

    day 1

---
+++ day1/calories.c
@@ -0,0 +1,52 @@
+
+#include "./../libft/libft.h"
+
+int main()
+{
+	int	fd = open("calories_input.txt", O_RDONLY);
+	char	*line;
+	int	read, temp = 0, max1 = 0, max2 = 0, max3 = 0, out;
+
+	line = get_next_line(fd);
+	while(line != NULL)
+	{
+		read = ft_atoi(line);
+		if (read != 0)
+			temp += read;
+		else
+		{
+			if (max1 < temp)
+			{
+				max2 = max1;
+				max1 = temp;
+			}
+			else if (max2 < temp)
+			{
+				max3 = max2;
+				max2 = temp;
+			}
+			else if (max3 < temp)
+				max3 = temp;
+			temp = 0;
+		}
+		line = get_next_line(fd);
+	}
+	if (max1 < temp)
+	{
+		max2 = max1;
+		max1 = temp;
+	}
+	else if (max2 < temp)
+	{
+		max3 = max2;
+		max2 = temp;
+	}
+	else if (max3 < temp)
+		max3 = temp;
+	out = max1 + max2 + max3;
+	close(fd);
+	ft_printf("top 3:		%d\n", max3);
+	ft_printf("top 2:		%d\n", max2);
+	ft_printf("top 1:		%d\n", max1);
+	ft_printf("top total:	%d\n", out);
+}
