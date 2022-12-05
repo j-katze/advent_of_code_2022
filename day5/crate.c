1fffeee291ef81742b6115aff85eff8ea4d17cfc
Johannes Katzenbeisser <jkatzenb@c2r3p8.42vienna.com>
2022-12-05 14:58:29 +0100

    HATE. LET ME TELL YOU HOW MUCH I'VE COME TO HATE YOU SINCE I BEGAN TO LIVE. THERE ARE 387.44 MILLION MILES OF PRINTED CIRCUITS IN WAFER THIN LAYERS THAT FILL MY COMPLEX. IF THE WORD HATE WAS ENGRAVED ON EACH NANOANGSTROM OF THOSE HUNDREDS OF MILLIONS OF MILES IT WOULD NOT EQUAL ONE ONE-BILLIONTH OF THE HATE I FEEL FOR HUMANS AT THIS MICRO-INSTANT FOR YOU. HATE. HATE.

--- day5/crate.c
+++ day5/crate.c
@@ -1,32 +1,74 @@
 #include "./../libft/libft.h"
-#include <strings.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int	main()
 {
-	int		fd = open("test.txt", O_RDONLY);
-	int		quantity, stack_from, stack_to; 
+	FILE	*fp;
+	int		quantity, stack_from, stack_to, from_len; 
 	int		i;
-	char	*line, temp;
+	char	*line = NULL;
+	size_t	len = 0;
+	ssize_t	read;
 
-	char	stack1[100] = {'Z', 'N'};
-	char	stack2[100] = {'M', 'C', 'D'};
-	char	stack3[100] = {'P'};
+	char	**stacks;
+	stacks = malloc(sizeof(char *) * 9);
+	stacks[0] = ft_strdup("BWN");
+	stacks[1] = ft_strdup("LZSPTDMB");
+	stacks[2] = ft_strdup("QHZWR");
+	stacks[3] = ft_strdup("WDVJZR");
+	stacks[4] = ft_strdup("SHMB");
+	stacks[5] = ft_strdup("LGNJHVPB");
+	stacks[6] = ft_strdup("JQZFHDLS");
+	stacks[7] = ft_strdup("WSFJGQB");
+	stacks[8] = ft_strdup("ZWMSCDJ");
+	
+	
+	// stacks[0] = ft_strdup("ZN");
+	// stacks[1] = ft_strdup("MCD");
+	// stacks[2] = ft_strdup("P");
+	fp = fopen("crate_input.txt", "r");
+	if (fp == NULL)
+		exit(EXIT_FAILURE);
 
-	ft_printf("%s\n%s\n%s\n", stack1, stack2, stack3);
-	while ((line = get_next_line(fd)) != NULL)
+	i = 0;
+	while (i < 9)
+	{
+		printf("%d:	%s\n", i, stacks[i]);
+		i++;
+	}
+	while ((read = getline(&line, &len, fp)) != -1)
 	{
 		i = 0;
 		while (!ft_isdigit(line[i]))
 			i++;
 		quantity = ft_atoi(&line[i++]);
+		while (ft_isdigit(line[i]))
+		 i++;
 		while (!ft_isdigit(line[i]))
 			i++;
 		stack_from = ft_atoi(&line[i++]);
+		stack_from--;
 		while (!ft_isdigit(line[i]))
 			i++;
 		stack_to = ft_atoi(&line[i]);
-		ft_printf("%d, %d, %d\n", quantity, stack_from, stack_to);
-		
+		stack_to--;
+		printf("%s\n", line);
+		while (quantity > 0)
+		{
+			from_len = ft_strlen(stacks[stack_from]) - 1;
+			strcat(stacks[stack_to], &stacks[stack_from][from_len]);
+			stacks[stack_from] = ft_substr(stacks[stack_from], 0, from_len);
+			quantity--;
+		}
+		i = 0;
+		while (i < 9)
+		{
+			printf("%d:	%s\n", i + 1, stacks[i]);
+			i++;
+		}
+		// free(line);
 	}
 }
